5b4cc8576002a9c3005ce6c295a4bfb6a456ae80
Drake906 <wankumbusiwale906@outlook.com>
2023-10-06 01:27:54 +0200

    finished project

---
+++ android/app/src/main/kotlin/com/example/appmobileexpensetracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appmobileexpensetracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/root_app.dart
@@ -0,0 +1,94 @@
+import 'package:appmobileexpensetracker/pages/profile_page.dart';
+import 'package:appmobileexpensetracker/pages/stats_page.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+
+import '../theme/colors.dart';
+import 'budget_page.dart';
+import 'create_budge_page.dart';
+import 'daily_page.dart';
+
+class RootApp extends StatefulWidget {
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+  List<Widget> pages = [
+    DailyPage(),
+    StatsPage(),
+    BudgetPage(),
+    ProfilePage(),
+    CreatBudgetPage()
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: getBody(),
+        bottomNavigationBar: getFooter(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              selectedTab(4);
+            },
+            child: Icon(
+              Icons.add,
+              size: 25,
+            ),
+            backgroundColor: Colors.blue
+            //params
+            ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.centerDocked);
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: pageIndex,
+      children: pages,
+    );
+  }
+  Widget getFooter() {
+    List<IconData> iconItems = [
+      Icons.calendar_month,
+      Icons.show_chart_rounded,
+      Icons.wallet,
+      Icons.person,
+    ];
+
+    return AnimatedBottomNavigationBar(
+      activeColor: primary,
+      splashColor: secondary,
+      inactiveColor: Colors.black.withOpacity(0.5),
+      icons: iconItems,
+      activeIndex: pageIndex,
+      gapLocation: GapLocation.center,
+      notchSmoothness: NotchSmoothness.softEdge,
+      leftCornerRadius: 10,
+      iconSize: 25,
+      rightCornerRadius: 10,
+      onTap: (index) {
+        selectedTab(index);
+      },
+      //other params
+    );
+  }
+
+  selectedTab(index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+}
